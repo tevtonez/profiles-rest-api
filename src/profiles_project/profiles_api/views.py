63dfcbd31f587515a231fcf29eddde05d5d8f425
tevtonez <tevtonez@gmail.com>
2017-10-15 22:48:24 +0200

    add create, retreive, udpate, partial_update, destroy method to ViewSet

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -58,9 +58,13 @@ class HelloApiView(APIView):
     return Response({'method':'delete'})
 
 
-
+########################
+#        ViewSets
+########################
 class HelloViewSet(viewsets.ViewSet):
   """Test API ViewSet"""
+
+  serializer_class = serializers.HelloSerializer
 
   def list(self, request):
     """Return hello msg"""
@@ -73,3 +77,31 @@ class HelloViewSet(viewsets.ViewSet):
 
     return Response({'message':'Hello from ViewSet!', 'apiview_list':apiview_list})
 
+  def create(self, request):
+    """Creates a new hello msg"""
+
+    serializer = serializers.HelloSerializer(data=request.data)
+
+    if serializer.is_valid():
+      name = serializer.data.get('name')
+      message = "Hello {}".format(name)
+
+      return Response({'message':message})
+    else:
+      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+  def retrieve(self, request, pk=None):
+    """Handles getting an object by PK"""
+    return Response({'http_method':'GET'})
+
+  def update(self, request, pk=None):
+    """Handles updating an object by PK"""
+    return Response({'http_method':'PUT'})
+
+  def partial_update(self, request, pk=None):
+    """Handles updating part of an object by PK"""
+    return Response({'http_method':'PATCH'})
+
+  def destroy(self, request, pk=None):
+    """Deletes an object by PK"""
+    return Response({'http_method':'DELETE'})
