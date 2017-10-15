c7c54ac331ef2a912c7a8f6288182cd48ebfa526
tevtonez <tevtonez@gmail.com>
2017-10-15 21:47:59 +0200

    create a test API endpoint as APIView

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.views import Response
 
 # Create your views here.
+
+class HelloApiView(APIView):
+  """Test API"""
+  def get(self, request, format=None):
+    """Returns a list of APIview features"""
+    apiview_list=[
+      'uses HTTP methods as functions',
+      'similar to traditional Django Views',
+      'Gives you the most control over your logic',
+      'is mapped manually to URL',
+    ]
+
+    return Response({'message':'Hello!', 'apiview_list':apiview_list})
