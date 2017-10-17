4a538f92c3b30d96d7dd0ad997449b58c58defc4
tevtonez <tevtonez@gmail.com>
2017-10-17 17:05:28 +0200

    add model for storing profile feed items, create migrations and add model to dj admin

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -60,3 +60,13 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
       return self.email
 
+
+class ProfileFeedItem(models.Model):
+  """Profile status update"""
+
+  user_profile = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
+  status_text = models.CharField(max_length=250)
+  created_on = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return self.status_text
